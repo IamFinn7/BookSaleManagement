9285edc14ce27104ca89d87c28fb46ebba6eda06
Finn <cpn2003.py@gmail.com>
2024-04-16 20:52:59 +0700

    Add project files.

---
+++ BookSaleManagement/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookSaleManagement.Models;
+
+namespace BookSaleManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookSaleManagementEntities bookSale_db = new BookSaleManagementEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult OutStandingBooks()
+        {
+            var listBooks = bookSale_db.Saches.Take(4).ToList();
+            return PartialView(listBooks);
+        }
+    }
+}
---
+++ BookSaleManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookSaleManagement.MvcApplication" Language="C#" %>
